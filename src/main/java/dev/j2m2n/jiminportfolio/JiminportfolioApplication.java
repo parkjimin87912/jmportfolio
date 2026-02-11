a5fb5116e170f61a04653942f60573b8cf52eebd
pjm6857 <pjm6857@gmail.com>
2026-02-11 10:19:52 +0900

    스브링부트로 변환

---
+++ src/main/java/dev/j2m2n/jiminportfolio/JiminportfolioApplication.java
@@ -0,0 +1,13 @@
+package dev.j2m2n.jiminportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiminportfolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiminportfolioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/j2m2n/jiminportfolio/JiminportfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.j2m2n.jiminportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiminportfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
